710be8b3351a6bcea614750b8dd10087d26d14ab
Dmitry Nourell <hi@imcatwhocode.dev>
2023-03-16 16:19:38 +0300

    Release 1.2.0

--- scripts/build-shebang.js
+++ scripts/build-shebang.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync, writeFileSync, chmodSync } = require('fs');
 const { join } = require('path');
 
+// Append shebang
 const path = join(__dirname, '..', 'dest', 'index.js');
 const contents = readFileSync(path, 'utf-8');
 writeFileSync(path, `#!/usr/bin/env node\n${contents}`, 'utf-8');
+
+// Set executable chmod
+chmodSync(path, 0775);

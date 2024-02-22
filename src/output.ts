f3d6121cf55084150ff6028f2d8af0ce2c7c5a16
Dmitry Nourell <hi@imcatwhocode.dev>
2024-02-23 01:24:18 +0300

    GH-10: Better log messages and formatting

--- src/output.ts
+++ src/output.ts
@@ -2,4 +2,14 @@ import { pino } from 'pino';
 
 export const logger = pino({
   name: 'envienc',
+  transport:
+    process.env.LOG_JSON === 'true'
+      ? undefined
+      : {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+          },
+        },
 });
